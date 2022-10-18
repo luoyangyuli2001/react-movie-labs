9df5b79f09fbf00a636883350137412074b1637e
LuoYang <luoyangyuli2001@gmail.com>
2022-10-18 10:42:50 +0100

    E2E tests - Filtering by title and genre work separately.

--- movies/cypress/support/e2e.js
+++ movies/cypress/support/e2e.js
@@ -16,5 +16,10 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+export const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+export const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
